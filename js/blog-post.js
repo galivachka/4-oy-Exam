2f2f6689be62949326a74ff90359b09a805c8f7a
galivachka <@muhammadusmonov182@gmail.com>
2023-04-14 16:01:25 +0500

    full

--- js/blog-post.js
+++ js/blog-post.js
@@ -9,7 +9,7 @@ let box2 = document.querySelector('.post');
 
 
 
-let url = `https://reqres.in/api/users`;
+let url = `https://reqres.in/api/users/`;
 
 fetch(url).then((response) => response.json()).then((data) => {
     let dataslice = data.data.slice(0, 1);
@@ -17,32 +17,32 @@ fetch(url).then((response) => response.json()).then((data) => {
     let datamap = dataslice.map((user) => {
         const { avatar, first_name, title,email,   } = user;
         return `
-<div class="section-box">
-<div class="section-box-forimg">
-<img class="image-avatar" src=${avatar} alt="...">
-</div>
-<div class="section-about">
-    <div class="forname">
-    <h3 class="name">${first_name}</h3>
-    <h3 class="title"> ${title}</h3>
-    </div>
-    <div class="for-data">
-    <a class="data" href="mailto:">${email}</a>
-    </div>
-</div>
-</div>
-
-`
+        <div class="section-box">
+        <div class="section-box-forimg">
+        <img class="image-avatar" src=${avatar} alt="...">
+        </div>
+        <div class="section-about">
+        <div class="forname">
+        <h3 class="name">${first_name}</h3>
+        <h3 class="title"> ${title}</h3>
+        </div>
+        <div class="for-data">
+        <a class="data" href="mailto:">${email}</a>
+        </div>
+        </div>
+        </div>
+        
+        `
     })
     sectionbox.innerHTML = datamap.join('');
-
+    
 })
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    
     let name = document.getElementById('name').value;
-   
+    
     let title = document.getElementById('title').value;
     let email = document.getElementById('mail').value;
     let image = document.getElementById('file').files[0];
@@ -62,56 +62,64 @@ form.addEventListener('submit', (e) => {
         }).then((response) => response.json()).then((data) => {
             console.log(data);
             box2.innerHTML = `
-    <div class="section-box">
-    <div class="section-box-forimg">
-    <img class="image-avatar" src='${imageurl}' alt="...">
-    </div>
-    <div class="section-about">
-        <div class="forname">
-        <h3 class="name">${data.firstname}</h3>
-        <h3 class="title"> ${data.title}</h3>
-        </div>
-        <div class="for-data">
-        <a class="data" href="mailto:">${data.email}</a>
-        <button class="mydelete" data-id="${data.id}">Remove</button>
-        <button class="myedit" data-id="${data.id}">Edit</button>
-        
-    </div>
-    </div>
-    
-    `
-    box2.addEventListener('click',(e)=>{
-        if(e.target.classList.contains('box__btn')){
-            const id = e.target.dataset.id;
-            deleteUser(id).then(()=>{
-                const anyth = e.target.closest('div');
-                anyth.remove();
-            }) 
-        }
-        })
-    async function deleteUser (id){
-    try {
-    const response = await fetch`${url}/${id}`;
-    method:'Delete'
-    if(response.ok){
-        return response;
-        
-    } 
-    } catch (error) {
-    console.log(error)
-    }
- }
-            box.appendChild(box2)
+            <div class="section-box">
+            <div class="section-box-forimg">
+            <img class="image-avatar" src='${imageurl}' alt="...">
+            </div>
+            <div class="section-about">
+            <div class="forname">
+            <h3 class="name">${data.firstname}</h3>
+            <h3 class="title"> ${data.title}</h3>
+            </div>
+            <div class="for-data">
+            <a class="data" href="mailto:">${data.email}</a>
+            <button class="myedit" data-id="${data.id}">Edit</button>
+            <button class="mydelete" data-id="${data.id}">Remove</button>
+            
+            </div>
+            </div>
+            
+            `
+            // box2.addEventListener('click',(e)=>{
+            //     if(e.target.classList.contains('box__btn')){
+            //         const id = e.target.dataset.id;
+            //         deleteUser(id).then(()=>{
+            //             const anyth = e.target.closest('div');
+            //             anyth.remove();
+            //         }) 
+            //     }
+            // })
+            // async function deleteUser (id){
+            //     try {
+            //         const response = await fetch`${url}/${id}`;
+            //         method:'Delete'
+            //         if(response.ok){
+            //             return response;
+                        
+            //         } 
+            //     } catch (error) {
+            //         console.log(error)
+            //     }
+            // }
+            // box.appendChild(box2)
         })
     }
     box.innerHTML = '';
-
+    
     form.reset();
 })
 
 
+remove.addEventListener('submit', (e) => {
+   console.log(document.querySelector('.section-box').closest());
+
+    fetch(url + "id", {
+    method: 'DELETE',
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+})
 
 
 
-    
 
--- start-up/start-up.js
+++ start-up/start-up.js
@@ -1,85 +1,87 @@
-let myUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e7c727b8a37449d8bd90b33bf220b129'
+let fetchURL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e7c727b8a37449d8bd90b33bf220b129'
 const input = document.querySelector(".search__inp");
 let currentPage = 1;
 
-const paginationWrapper = document.querySelector('.pagination-wrapper');
+const paginationWrapper = document.querySelector('.page-wrapper');
 
 
 async function myFetchData() {
     ``;
-
+    
     try {
-        const searchingBox = document.querySelector(".search__box");
-        const myInp = document.querySelector(".search__inp").value;
-
-        if (myInp) {
-            myUrl = `https://newsapi.org/v2/everything?q=${myInp}&apiKey=e7c727b8a37449d8bd90b33bf220b129`;
+        const searchWrapper = document.querySelector(".searching__box");
+        const inputValue = document.querySelector(".search__inp").value;
+        
+        if (inputValue) {
+            fetchURL = `https://newsapi.org/v2/everything?q=${inputValue}&apiKey=e7c727b8a37449d8bd90b33bf220b129`;
         } else {
-            myUrl = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e7c727b8a37449d8bd90b33bf220b129&page=${currentPage}`;
+            fetchURL = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e7c727b8a37449d8bd90b33bf220b129&page=${currentPage}`;
         }
-
-        let response = await fetch(myUrl);
-        let data = await response.json();
-
-        let totalPages = data.totalResults > 4 ? 4 : data.totalResults;
-        searchingBox.innerHTML = '';
-
-        if (data.articles.length === 0) {
-            const notFound = document.createElement('div');
-            notFound.textContent = 'No Information...'
-            searchingBox.appendChild(notFound);
-        } else {
-            data.articles.slice(0, 6).map((fetches) => {
-                const wrapperDiv = document.createElement('div');
-                wrapperDiv.classList.add('wrapper__item');
-                const wrapperImg = document.createElement('img');
-                const wrapperDiv2 = document.createElement('div');
-                wrapperDiv2.classList.add("search-box-text");
-                const wrapperSpan = document.createElement('span');
-                const wrappperTitle = document.createElement('h2');
-                const wrapperP = document.createElement('p');
-
-                // wrapperImg.src = fetches.urlToImage
-                if (wrapperImg.src == null) {
-                    wrapperImg.src = 'https://avatars.mds.yandex.net/get-verba/1540742/2a00000180d7fa047e2e09e71e665301c310/cattouch'
-                } else {
-                    wrapperImg.src = fetches.urlToImage
-
-                }
-                wrapperImg.alt = fetches.title;
-                wrapperImg.classList.add('wrapper__img')
-                wrapperSpan.textContent = fetches.author;
-                wrapperSpan.classList.add('wrapper__span')
-                wrappperTitle.textContent = fetches.title;
-                wrappperTitle.classList.add('wrapper__h2')
-                wrapperP.textContent = fetches.description;
-
-                wrapperDiv2.append(wrapperSpan, wrappperTitle, wrapperP);
-
-                wrapperDiv.append(wrapperImg, wrapperDiv2);
-
-                searchingBox.append(wrapperDiv);
-            })
-            paginationWrapper.innerHTML = '';
-            const buttonsDiv = document.querySelector('.buttons__div')
-            if (totalPages > 1) {
-                for (let s = 1; s <= totalPages; s++) {
-                    const button = document.createElement('button');
-                    button.innerText = s;
-
-                    if (currentPage === s) {
-                        button.classList.add('button-active')
+        fetch(fetchURL)
+        .then(res => res.json())
+        .then(data => {
+            let totalPages = data.totalResults > 3 ? 3 : data.totalResults;
+            searchWrapper.innerHTML = '';
+            
+            if (data.articles.length === 0) {
+                const noInfo = document.createElement('div');
+                noInfo.textContent = 'No Info...'
+                searchWrapper.appendChild(noInfo);
+            } else {
+                data.articles.slice(0, 6).map((fetches) => {
+                    const wrap1 = document.createElement('div');
+                    wrap1.classList.add('wrapper__item');
+                    const wrapperImg = document.createElement('img');
+                    const wrap2 = document.createElement('div');
+                    wrap2.classList.add("search-box-text");
+                    const wrapSpan = document.createElement('span');
+                    const wrapTitle = document.createElement('h2');
+                    const wrapP = document.createElement('p');
+                    
+        
+                    if (wrapperImg.src == null) {
+                        wrapperImg.src = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.edmunds.com%2Fbmw%2F3-series%2F&psig=AOvVaw1_3qohsxkJPM5GIqcrWlur&ust=1681556102881000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKjAqeWaqf4CFQAAAAAdAAAAABAJ'
+                    } else {
+                        wrapperImg.src = fetches.urlToImage
+                        
                     }
-                    button.addEventListener('click', () => {
-                        currentPage = s;
-                        myFetchData();
-                    })
-                    button.classList.add('btnact')
-                    searchingBox.appendChild(button);
+                    wrapperImg.alt = fetches.title;
+                    wrapperImg.classList.add('wrapper__img')
+                    wrapSpan.textContent = fetches.author;
+                    wrapSpan.classList.add('wrapper__span')
+                    wrapTitle.textContent = fetches.title;
+                    wrapTitle.classList.add('wrapper__h2')
+                    wrapP.textContent = fetches.description;
+                    
+                    wrap2.append(wrapSpan, wrapTitle, wrapP);
+                    
+                    wrap1.append(wrapperImg, wrap2);
+                    
+                    searchWrapper.append(wrap1);
+                })
+                paginationWrapper.innerHTML = '';
+                const btnDiv = document.querySelector('.btn__div')
+                if (totalPages > 1) {
+                    for (let s = 1; s <= totalPages; s++) {
+                        const button = document.createElement('button');
+                        button.innerText = s;
+                        
+                        if (currentPage === s) {
+                            button.classList.add('button-active')
+                        }
+                        button.addEventListener('click', () => {
+                            currentPage = s;
+                            myFetchData();
+                        })
+                        button.classList.add('btnact')
+                        searchWrapper.appendChild(button);
+                    }
                 }
             }
-        }
-
+        })
+        
+        
+        
     } catch (error) {
         console.error(error);
     }
